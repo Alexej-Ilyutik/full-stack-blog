80fab6f8cb13a9fc59fa7ccbc0cdb36c8698b1c9
Alexej Ilyutik <alexej.ilyutik@gmail.com>
2022-12-20 16:51:11 +0300

    feat: connect momngo DB

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
+import { validationResult } from 'express-validator';
+
+import { registerValidation } from './validations/auth.js';
+import UserModel from './models/User.js';
 
 mongoose.set('strictQuery', false);
 mongoose
   .connect(
-    'mongodb+srv://Alexej:ASD_Lut_88@cluster0.lt4akvh.mongodb.net/?retryWrites=true&w=majority'
+    'mongodb+srv://Alexej:ASD_Lut_88@cluster0.lt4akvh.mongodb.net/blog?retryWrites=true&w=majority'
   )
   .then(() => console.log('DB OK'))
   .catch((err) => console.log('DB Error', err));
@@ -14,22 +19,40 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (request, response) => {
-  response.send('fth888fljkh');
-});
+// app.get('/', (request, response) => {
+//   response.send('fth888fljkh');
+// });
 
-app.post('/auth/login', (request, response) => {
-  console.log(request.body);
+app.post('/auth/register', registerValidation, async (request, response) => {
+  const errors = validationResult(request);
 
-  const token = jwt.sign(
-    {
-      email: request.body.email,
-      fullName: 'Alexej Ilyutik',
-    },
-    'secret8448'
-  );
+  if (!errors.isEmpty()) {
+    return response.status(400).json(errors.array());
+  }
 
-  response.json({ success: true, token });
+  const password = request.body.password;
+  const salt = await bcrypt.genSalt(10);
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  const doc = new UserModel({
+    email: request.body.email,
+    passwordHash,
+    fullName: request.body.fullName,
+    avatarUrl: request.body.avatarUrl,
+  });
+
+  const user = await doc.save();
+
+  response.json(user);
+  // console.log(request.body);
+  // const token = jwt.sign(
+  //   {
+  //     email: request.body.email,
+  //     fullName: 'Alexej Ilyutik',
+  //   },
+  //   'secret8448'
+  // );
+  // response.json({ success: true, token });
 });
 
 app.listen(3008, (err) => {
