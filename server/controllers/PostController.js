541d177b7eb36d81db4661ee863e3829f6df0813
Alexej Ilyutik <alexej.ilyutik@gmail.com>
2022-12-29 14:16:54 +0300

    feat: get one post

--- server/controllers/PostController.js
+++ server/controllers/PostController.js
@@ -14,6 +14,47 @@ export const getAllPosts = async (request, response) => {
   }
 };
 
+export const getPost = async (request, response) => {
+  try {
+    const postId = request.params.id; //get id from route
+    PostModel.findOneAndUpdate(
+      { _id: postId },
+      {
+        $inc: {
+          viewsCount: 1,
+        },
+      },
+      {
+        returnDocument: 'after',
+      },
+      (err, doc) => {
+        if (err) {
+          console.log(err);
+          const status = err.status || 500;
+          return response.status(status).json({
+            message: 'Failed to retrieve article!',
+          });
+        }
+
+        if (!doc) {
+          const status = err.status || 404;
+          return response.status(status).json({
+            message: 'Article not found!',
+          });
+        }
+
+        response.json(doc);
+      }
+    );
+  } catch (err) {
+    console.log(err);
+    const status = err.status || 500;
+    response.status(status).json({
+      message: 'Failed to retrieve article!',
+    });
+  }
+};
+
 export const createPost = async (request, response) => {
   try {
     const doc = new PostModel({
